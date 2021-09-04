1e47b9019069eb4cf752520ddf586395f65150cc
Prajwal Nayak <prajwal@Prajwals-MacBook-Air.local>
2021-09-04 13:38:29 +0530

    food tab all propper

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,14 @@ import HomeLayoutHOC from "./HOC/Home.HOC";
 
 //Component
 import Temp from "./Components/Temp";
+import Master from "./Components/master";
+
 
 function App() {
   return (
     <>
       <HomeLayoutHOC path="/" exact component= {Temp} /> 
+      <HomeLayoutHOC path="/:type" exact component= {Master} /> 
     </>
   );
 }
