3d9bc4c5dd0f1b4daf2da44c019ab46bc6f4b3c2
Prajwal Nayak <prajwal@Prajwals-MacBook-Air.local>
2021-09-07 17:00:20 +0530

    added nutition cards

--- client/src/Components/Nutrition/index.jsx
+++ client/src/Components/Nutrition/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import NutritionHeroCarousal from "./NutritionHeroCarousal";
 import NutritionCarousal from './NutritionCarousal';
+import NutritionCard from './NutritionCard';
 
 const Nutrition = () => {
     return (
@@ -9,6 +10,14 @@ const Nutrition = () => {
             <NutritionHeroCarousal/>
             <div className="my-6">
             <NutritionCarousal />
+            </div>
+            <div className="flex justify-between flex-wrap">
+            <NutritionCard bg="red" image = "https://dote.zmtcdn.com/prod/data/admin_assets/images/34e/384e52f7a60c840377ede182dcd3d34e_1614427090.png" />
+            <NutritionCard bg="red" image = "https://dote.zmtcdn.com/prod/data/admin_assets/images/34e/384e52f7a60c840377ede182dcd3d34e_1614427090.png" />
+            <NutritionCard bg="red" image = "https://dote.zmtcdn.com/prod/data/admin_assets/images/34e/384e52f7a60c840377ede182dcd3d34e_1614427090.png" />
+            <NutritionCard bg="red" image = "https://dote.zmtcdn.com/prod/data/admin_assets/images/34e/384e52f7a60c840377ede182dcd3d34e_1614427090.png" />
+            <NutritionCard bg="red" image = "https://dote.zmtcdn.com/prod/data/admin_assets/images/34e/384e52f7a60c840377ede182dcd3d34e_1614427090.png" />
+            <NutritionCard bg="red" image = "https://dote.zmtcdn.com/prod/data/admin_assets/images/34e/384e52f7a60c840377ede182dcd3d34e_1614427090.png" />
             </div>
         </>
     )
