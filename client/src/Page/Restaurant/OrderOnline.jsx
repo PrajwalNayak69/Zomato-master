c1fabedb617d20d12f028a6bcb21cd41d632ee72
Prajwal Nayak <prajwal@Prajwals-MacBook-Air.local>
2021-09-11 21:22:00 +0530

    add online order menu

--- client/src/Page/Restaurant/OrderOnline.jsx
+++ client/src/Page/Restaurant/OrderOnline.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 import FloatMenu from '../../Components/restaurant/OrderOnline/FloatMenu';
-
+import MenuListContainer from '../../Components/restaurant/OrderOnline/MenuListContainer';
 
 const OrderOnline = () => {
     return (
         <>
               <div className="w-full ">
-              <FloatMenu />
+                  <aside className="hidden md:flex flex-col border-r border-gray-200 overflow-y-scroll h-screen gap-3 w-1/4">
+
+                        <MenuListContainer />
+                        <MenuListContainer />
+                        <MenuListContainer />
+                        <MenuListContainer />
+                        <MenuListContainer />
+                        <MenuListContainer />
+                        <MenuListContainer />
+                        <MenuListContainer />
+                        <MenuListContainer />
+                        <MenuListContainer />
+                  </aside>
+                  <div className="w-full md:w-3/4">
+                  </div>
                </div>
+              <FloatMenu />
         </>
     )
 }
