5ff6d55371c957e246b0e531c680a70ee1606531
Prajwal Nayak <prajwal@Prajwals-MacBook-Air.local>
2021-10-11 22:48:42 +0530

    add menu images

--- client/src/Page/Restaurant/Overview.jsx
+++ client/src/Page/Restaurant/Overview.jsx
@@ -1,142 +1,188 @@
-import React from 'react'
-import { Link, useParams } from 'react-router-dom';
-import {IoMdArrowDropright} from "react-icons/io";
+import React, { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import { IoMdArrowDropright } from "react-icons/io";
+import Slider from "react-slick";
+import { useSelector, useDispatch } from "react-redux";
 import ReactStars from "react-rating-stars-component";
 
+// components
+import MenuCollection from "../../Components/restaurant/MenuCollection";
+import MenuSimilarRestaurantcard from "../../Components/restaurant/MenuRestaurantSimilarCard";
+import { NextArrow, PrevArrow } from "../../Components/CarousalArrow";
+import ReviewCard from "../../Components/restaurant/Reviews/reviewCard";
+import Mapview from "../../Components/restaurant/Mapview";
 
-//component
+import { getImage } from "../../Redux/Reducer/Image/Image.action";
+import { getReviews } from "../../Redux/Reducer/Reviews/review.action";
 
-import MenuCollection from '../../Components/restaurant/MenuCollection';
-import MenuRestaurantSimilarCard from '../../Components/restaurant/MenuRestaurantSimilarCard';
-import Reviewcard from '../../Components/restaurant/reviews/reviewcard';
-import MapView from '../../Components/restaurant/MapView';
+const Overview = () => {
+  const [menuImage, setMenuImages] = useState({ images: [] });
+  const [Reviews, setReviewss] = useState([]);
 
+  const { id } = useParams();
 
-//slider carousal
-import Slider from 'react-slick';
-import { NextArrow, PrevArrow } from '../../Components/CarousalArrow';
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
 
-const ratingChanged = (newRating) => {
+  const reduxState = useSelector(
+    (globalStore) => globalStore.restaurant.selectedRestaurant.restaurant
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (reduxState) {
+      dispatch(getImage(reduxState?.menuImage)).then((data) => {
+        const images = [];
+        data.payload.image.images.map(({ location }) => images.push(location));
+        setMenuImages(images);
+      });
+      dispatch(getReviews(reduxState?._id)).then((data) =>
+        setReviewss(data.payload.reviews)
+      );
+    }
+  }, []);
+
+  const ratingChanged = (newRating) => {
     console.log(newRating);
   };
 
-const Overview = () => {
-    const {id} = useParams();
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        initialSlide: 0,
-        nextArrow: <NextArrow />,
-        prevArrow: <PrevArrow />,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1,
-              infinite: true,
-             
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1,
-              infinite: true,
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1,
-              infinite: true,
-            }
-          }
-        ]
-      };
-      
+  const getLatLong = (mapAddress) => {
+    return mapAddress?.split(",").map((item) => parseFloat(item));
+  };
 
-    return (
-        <>
-           <div className="flex flex-col md:flex-row relative">
-               <div className="w-full md:w-8/12">
-                   <h2 className="font-semibold text-lg md:text-xl my-4 ">About this place</h2>
-                   <div className="flex justify-between items-center">
-                       <h4 className="text-lg font-medium">Menu</h4>
-                       <Link to={`/restaurant/${id}/menu`}>
-                        <span className="flex items-center gap-1 text-zomato-400">See all menu <IoMdArrowDropright /></span>
-                       </Link>
-                           </div>
-                       <div className="flex flex-wrap gap-3">
-                           <MenuCollection menutitle="menu" pages="3" image ={
-                             ["https://b.zmtcdn.com/data/menus/854/18698854/e9ee84cccb3208b9d4b032f4d5e7c879.jpg",
-                             "https://b.zmtcdn.com/data/menus/854/18698854/e9ee84cccb3208b9d4b032f4d5e7c879.jpg"
-                            ]}
-                           /> 
-                   </div>
-                   <h4 className="text-lg font-medium">Cuisine</h4>
-                   <div className="flex flex-wrap gap-2 ">
-                       <span className="border border-gray-600 text-blue-600 px-2 py-1 rounded-full">Biryani</span>
-                       <span className="border border-gray-600 text-blue-600 px-2 py-1 rounded-full">North Indian</span>
-                       <span className="border border-gray-600 text-blue-600 px-2 py-1 rounded-full">Seafood</span>
-                       <span className="border border-gray-600 text-blue-600 px-2 py-1 rounded-full">Chinese</span>
-                       <span className="border border-gray-600 text-blue-600 px-2 py-1 rounded-full">Desert</span>
-                   </div>
-               <div className="my-4">
-               <h4 className="text-lg font-medium">Average Cost</h4>
-               <h4>₹500 for two people (approx.)</h4>
-               <small className="text-gray-500">Exclusive of applicable taxes and charges, if any
+  return (
+    <>
+      <div className="flex flex-col md:flex-row relative">
+        <div className="w-full md:w-8/12">
+          <h2 className="font-semibold text-lg md:text-xl my-4">
+            About this place
+          </h2>
+          <div className="flex justify-between items-center">
+            <h4 className="text-lg font-medium">Menu</h4>
+            <Link to={`/restaurant/${id}/menu`}>
+              <span className="flex items-center gap-1 text-zomato-400">
+                See all menu <IoMdArrowDropright />
+              </span>
+            </Link>
+          </div>
+          <div className="flex flex-wrap gap-3 my-4">
+            <MenuCollection menuTitle="Menu" pages="3" image={menuImage} />
+          </div>
+          <h4 className="text-lg font-medium my-4">Cuisines</h4>
+          <div className="flex flex-wrap gap-2">
+            {reduxState?.cuisine.map((data) => (
+              <span className="border border-gray-600 text-blue-600 px-2 py-1 rounded-full">
+                {data}
+              </span>
+            ))}
+          </div>
+          <div className="my-4">
+            <h4 className="text-lg font-medium">Average Cost</h4>
+            <h6>₹{reduxState?.averageCost} for one order (approx.)</h6>
+            <small className="text-gray-500">
+              Exclusive of applicable taxes and charges, if any
+            </small>
+          </div>
+          <div className="my-4">
+            <h4 className="text-lg font-medium">Similar Restaurants</h4>
+            <div>
+              <Slider {...settings}>
+                <MenuSimilarRestaurantcard
+                  image="https://b.zmtcdn.com/data/pictures/chains/5/18711475/4be376adb66b75764946d00a7dcf9991_featured_v2.jpg?output-format=webp"
+                  title="tea"
+                />
+                <MenuSimilarRestaurantcard
+                  image="https://b.zmtcdn.com/data/pictures/chains/5/18711475/4be376adb66b75764946d00a7dcf9991_featured_v2.jpg?output-format=webp"
+                  title="tea"
+                />
+                <MenuSimilarRestaurantcard
+                  image="https://b.zmtcdn.com/data/pictures/chains/5/18711475/4be376adb66b75764946d00a7dcf9991_featured_v2.jpg?output-format=webp"
+                  title="tea"
+                />
+                <MenuSimilarRestaurantcard
+                  image="https://b.zmtcdn.com/data/pictures/chains/5/18711475/4be376adb66b75764946d00a7dcf9991_featured_v2.jpg?output-format=webp"
+                  title="tea"
+                />
+                <MenuSimilarRestaurantcard
+                  image="https://b.zmtcdn.com/data/pictures/chains/5/18711475/4be376adb66b75764946d00a7dcf9991_featured_v2.jpg?output-format=webp"
+                  title="tea"
+                />
+              </Slider>
+            </div>
+          </div>
+          <div className="my-4">
+            <h4 className="text-lg font-medium">
+              Rate your delivery experience
+            </h4>
+            <ReactStars
+              count={5}
+              onChange={ratingChanged}
+              size={24}
+              activeColor="#ffd700"
+            />
+            {Reviews.map((reviewData) => (
+              <ReviewCard {...reviewData} />
+            ))}
+          </div>
+          <div className="my-4 w-full  md:hidden flex flex-col gap-4">
+            <Mapview
+              title={reduxState?.name}
+              phno={`+91${reduxState?.contactNumber}`}
+              mapLocation={getLatLong(reduxState?.mapLocation)}
+              address={reduxState?.address}
+            />
+          </div>
 
-                How do we calculate cost for two?
-
-                </small>
-               </div>
-               <div className="my-4">
-               <h4 className="text-lg font-medium">Similar Restaurants</h4>
-               <Slider {...settings}>
-                <MenuRestaurantSimilarCard image="https://b.zmtcdn.com/data/pictures/chains/4/18698614/f325f5c3e4761fc65ae04435f70c816c_featured_v2.jpg" title="McDonald's"/>
-                <MenuRestaurantSimilarCard image="https://b.zmtcdn.com/data/pictures/chains/4/18698614/f325f5c3e4761fc65ae04435f70c816c_featured_v2.jpg" title="McDonald's"/>
-                <MenuRestaurantSimilarCard image="https://b.zmtcdn.com/data/pictures/chains/4/18698614/f325f5c3e4761fc65ae04435f70c816c_featured_v2.jpg" title="McDonald's"/>
-                <MenuRestaurantSimilarCard image="https://b.zmtcdn.com/data/pictures/chains/4/18698614/f325f5c3e4761fc65ae04435f70c816c_featured_v2.jpg" title="McDonald's"/>
-                <MenuRestaurantSimilarCard image="https://b.zmtcdn.com/data/pictures/chains/4/18698614/f325f5c3e4761fc65ae04435f70c816c_featured_v2.jpg" title="McDonald's"/>
-                <MenuRestaurantSimilarCard image="https://b.zmtcdn.com/data/pictures/chains/4/18698614/f325f5c3e4761fc65ae04435f70c816c_featured_v2.jpg" title="McDonald's"/>
-                </Slider>
-               </div>
-               <div className="">
-               <h4 className="text-lg font-medium">
-                   Rate your experience for</h4>
-                   <ReactStars
-                    count={5}
-                    onChange={ratingChanged}
-                    size={24}
-                    activeColor="#ffd700"
-                />,
-               
-               </div>
-               <div className="my-4 md:hidden flex-col gap-4 w-full">
-               <MapView title="sai's" phone="+918202570177" maplocation={[13.350726986617875, 74.7868899618099]} address="Opposite Syndicate Bank Head Office, Tiger Circle, Manipal, Eshwar Nagar, Manipal"/>
-               </div>
-                <div className="my-4 flex flex-col gap-4">
-                <Reviewcard />
-                <Reviewcard />
-                <Reviewcard />
-                <Reviewcard />
-                </div>
-               </div>
-               
-               <aside 
-               style={{ height: "fit-content "}}
-               className="hidden  md:flex md:w-2/5 sticky bg-white p-3 top-2 shadow-md rounded-xl  flex-col gap-4" >
-                    <MapView title="sai's" phone="+918202570177" maplocation={[13.350726986617875, 74.7868899618099]} address="Opposite Syndicate Bank Head Office, Tiger Circle, Manipal, Eshwar Nagar, Manipal"/>
-               </aside>
-               </div> 
-        </>
-    );
+          <div className="my-4 flex flex-col gap-4"></div>
+        </div>
+        <aside
+          style={{ height: "fit-content" }}
+          className="hidden md:flex md:w-4/12 sticky rounded-xl top-2 bg-white p-3 shadow-md flex flex-col gap-4"
+        >
+          <Mapview
+            title={reduxState?.name}
+            phno={`+91${reduxState?.contactNumber}`}
+            mapLocation={getLatLong(reduxState?.mapLocation)}
+            address={reduxState?.address}
+          />
+        </aside>
+      </div>
+    </>
+  );
 };
 
 export default Overview;
